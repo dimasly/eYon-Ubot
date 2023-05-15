0fd9bba7043a5ca516cfa79fb2ab4022ac342dea
Dimas <133459897+dimasly@users.noreply.github.com>
2023-05-16 02:34:23 +0800

    Update __main__.py

--- Geez/__main__.py
+++ Geez/__main__.py
@@ -10,13 +10,13 @@ from Geez.modules.basic.heroku import geez_log
 
 
 MSG_ON = """
-**Geez Pyro Userbot**
+**eYon Pyro Userbot**
 ╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
 **Userbot Version -** `{}`
 **Geez Library Version - `{}`**
 **Ketik** `{}geez` **untuk Mengecheck Bot**
 ╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
-©️2023 Geez|RAM Projects
+©️2023 eYon|RAM Projects
 """
 
 async def main():
@@ -44,6 +44,6 @@ async def main():
 
 
 if __name__ == "__main__":
-    LOGGER("Geez").info("Starting Geez Pyro Userbot")
+    LOGGER("Geez").info("Starting eYon Pyro Userbot")
     install()
     LOOP.run_until_complete(main())
